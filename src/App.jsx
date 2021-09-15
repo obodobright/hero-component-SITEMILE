1b375ae66c6005c59cb7a5f68b6c393ac9244320
Obodo Bright <Obodobright0@gmail.com>
2021-09-15 16:20:54 +0100

    iniatial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <React.Fragment className="App">
+     <Container>
+      <h1>My page</h1>
+     </Container>
+    </React.Fragment>
+  );
+}
+
+const Container = styled.div``
+export default App;
